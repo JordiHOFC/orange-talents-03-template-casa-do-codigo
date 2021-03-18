4748f14f48b2aa3e1ff7f1aadf45626b770f5413
jordihofc <jordi.silva@ufv.br>
2021-03-18 15:35:46 -0300

    refatorando com os ajustes apos finalizar tarefa

--- src/main/java/br/com/zup/casadocodigo/Autor/Autor.java
+++ src/main/java/br/com/zup/casadocodigo/Autor/Autor.java
@@ -1,6 +1,12 @@
 package br.com.zup.casadocodigo.Autor;
 
+import org.springframework.stereotype.Service;
+
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
@@ -22,7 +28,7 @@ public class Autor {
 
     public Autor(){}
 
-    public Autor(String nome, String email, String descricao) {
+    public Autor(@NotBlank String nome, @NotBlank @Email String email, @NotBlank @Size(max = 400) String descricao) {
         this.nome = nome;
         this.email = email;
         this.descricao = descricao;
--- src/main/java/br/com/zup/casadocodigo/Autor/AutorForm.java
+++ src/main/java/br/com/zup/casadocodigo/Autor/AutorForm.java
@@ -1,31 +1,26 @@
 package br.com.zup.casadocodigo.Autor;
 
-import org.hibernate.validator.constraints.Length;
-
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class AutorForm {
     @NotBlank
     private String nome;
-    @NotBlank @Length(max = 400)
+    @NotBlank @Size(max = 400)
     private String descricao;
     @NotBlank @Email
     private String email;
+
+    public AutorForm(@NotBlank String nome, @NotBlank @Size(max = 400) String descricao, @NotBlank @Email String email) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.email = email;
+    }
 
     public Autor converter(){
         return new Autor(this.nome,this.email, this.descricao);
     }
 
-    public String getNome() {
-        return nome;
-    }
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 }
